a87f5e6849ec3df1f45f3129d3d9bb14dae4079d
wmetz-FPSS <wmetz@fpsharedservices.org>
2018-02-05 18:51:23 -0500

    Indecision App completed

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -67,14 +67,16 @@ export default class IndecisionApp extends React.Component {
               hasOptions={this.state.options.length > 0}
               handlePick={this.handlePick}
               />
-            <Options 
-              options={this.state.options}
-              handleDeleteOptions = {this.handleDeleteOptions}
-              handleDeleteOption = {this.handleDeleteOption}
-            />
-            <AddOption
-              handleAddOption = {this.handleAddoption}
-            />
+              <div className="widget">
+                <Options 
+                options={this.state.options}
+                handleDeleteOptions = {this.handleDeleteOptions}
+                handleDeleteOption = {this.handleDeleteOption}
+                />
+                <AddOption
+                  handleAddOption = {this.handleAddoption}
+                />
+              </div>
           </div>
           <OptionModal
             selectedOption = {this.state.selectedOption}
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Option from './Option'
 const Options = (props) => (
       <div>
-        <button 
+        <div className="widget-header">
+          <h3 className="widget-header__title">Your Options</h3>
+          <button 
           className="button button--link"
           onClick={props.handleDeleteOptions}
           >
           Remove All
-        </button>
-        {props.options.length === 0 && <p> Please Add an Option to get Started </p>}
+          </button>
+
+        </div>
+
+        {props.options.length === 0 && <p className="widget__message"> Please Add an Option to get Started </p>}
       {
-        props.options.map((option) => (
+        props.options.map((option, index) => (
           <Option 
             key={option} 
             optionText={option}
+            count={index + 1}
             handleDeleteOptions = {props.handleDeleteOption}
             />
         ))
